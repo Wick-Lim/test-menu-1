92da8fcd9a801e834b24df3241016114a8236dfd
임준혁 <wick@rsquare.co.kr>
2023-12-14 10:22:22 +0900

    base commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
-import { FC } from 'react';
-import loadable from '@loadable/component';
-import { ThemeProvider } from "@emotion/react";
-import { CssBaseline } from "@mui/material";
-import theme from "./theme";
-
-const Container = loadable(() => import('@mui/material/Container'));
-const Box = loadable(() => import('@mui/material/Box'));
-const Typography = loadable(() => import('@mui/material/Typography'));
+import { FC } from "react";
 
 interface App extends FC {
   menus: any[];
 }
 
 const MyApp: App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Container maxWidth="sm">
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h4" gutterBottom>
-            Material UI Vite.js example in TypeScript
-          </Typography>
-        </Box>
-      </Container>
-    </ThemeProvider>
-  );
-}
+  return <h1>Material UI Vite.js example in TypeScript</h1>;
+};
 
 MyApp.menus = [
-  "appbar", "button", "card", "checkbox", "dialog", "drawer", "form", "grid", "icon", "input", "list", "menu", "paper", "progress", "select", "slider", "snackbar", "stepper", "table", "tabs", "tooltip", "typography"
-]
+  "appbar",
+  "button",
+  "card",
+  "checkbox",
+  "dialog",
+  "drawer",
+  "form",
+  "grid",
+  "icon",
+  "input",
+  "list",
+  "menu",
+  "paper",
+  "progress",
+  "select",
+  "slider",
+  "snackbar",
+  "stepper",
+  "table",
+  "tabs",
+  "tooltip",
+  "typography",
+];
 
 export default MyApp;
