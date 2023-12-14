5770b034e5c8fc527624cf4f212ff6e4d6febd1a
임준혁 <wick@rsquare.co.kr>
2023-12-14 10:53:10 +0900

    base commit

--- src/Service.tsx
+++ src/Service.tsx
@@ -1,16 +1,17 @@
-import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import MyApp from "./App";
 
 class XInbound extends HTMLElement {
-  static component: string = "x-inbound";
   connectedCallback() {
-    const mountPoint = document.createElement("span");
-    this.attachShadow({ mode: "open" }).appendChild(mountPoint);
-
-    const root = ReactDOM.createRoot(mountPoint);
+    const shadow = this.attachShadow({ mode: "open" });
+    const root = ReactDOM.createRoot(shadow);
     root.render(<MyApp />);
   }
 }
+
 customElements.define("x-inbound", XInbound);
-export default XInbound;
+
+export default {
+  Name: "x-inbound",
+  Component: XInbound,
+};
