5314cac2a402bd6cc2661a805dc4934933725faf
임준혁 <wick@rsquare.co.kr>
2023-12-14 10:46:45 +0900

    base commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,14 @@
-import { Typography } from "@mui/material";
+import { ThemeProvider, Typography } from "@mui/material";
 import { FC } from "react";
+import theme from "./theme";
 // import "./App.css";
 
-interface App extends FC {
-  menus: any[];
-}
-
-const MyApp: App = () => {
-  return <Typography>Material UI Vite.js example in TypeScript</Typography>;
+const MyApp: FC = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Typography color="primary">Material UI Vite.js example in TypeScript</Typography>
+    </ThemeProvider>
+  );
 };
-
-MyApp.menus = [
-  "appbar",
-  "button",
-  "card",
-  "checkbox",
-  "dialog",
-  "drawer",
-  "form",
-  "grid",
-  "icon",
-  "input",
-  "list",
-  "menu",
-  "paper",
-  "progress",
-  "select",
-  "slider",
-  "snackbar",
-  "stepper",
-  "table",
-  "tabs",
-  "tooltip",
-  "typography",
-];
 
 export default MyApp;
