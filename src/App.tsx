0e2cfa94c2e614377c3ca2fd5b36ee272354ba09
임준혁 <wick@rsquare.co.kr>
2023-12-14 14:08:07 +0900

    base commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { Typography } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Typography variant="h1">Hello World</Typography>
+      <BrowserRouter basename="/inbound">
+        <Routes>
+          <Route
+            path="/"
+            element={<Typography variant="h1">Home</Typography>}
+          />
+          <Route
+            path="/about"
+            element={<Typography variant="h1">About</Typography>}
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
